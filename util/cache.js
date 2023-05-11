d937ab977852c80e2033029ef3d8795b480b490f
liwei <liwei-8466@qq.com>
2023-05-11 18:11:42 +0800

    fix: cache impl

--- util/cache.js
+++ util/cache.js
@@ -1,18 +1,3 @@
-let releaseCache = null
-
 export default async function getLatestRelease() {
-  let data
-  if (releaseCache) {
-    data = releaseCache
-  } else {
-    console.log("fetching latest release")
-    data = await (await fetch('https://api.github.com/repos/GopeedLab/gopeed/releases/latest')).json()
-    releaseCache = data
-    // cache 1 hour
-    setTimeout(() => {
-      releaseCache = null
-    }, 1000 * 60 * 60)
-  }
-  return data
+  return await (await fetch('https://api.github.com/repos/GopeedLab/gopeed/releases/latest', { next: { revalidate: 300 } })).json()
 }
-
