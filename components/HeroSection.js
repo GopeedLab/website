121deb4c171a4ba029aac66e5503e8e2fffd13ca
liwei <liwei-8466@qq.com>
2025-02-25 16:31:26 +0800

    feat: update github cdn

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -49,7 +49,7 @@ const HeroSection = ({ release }) => {
 
     const getDownloadCdnUrl = (rawUrl) => {
       if (!githubAccessible) {
-        return `https://mirror.ghproxy.com/${rawUrl.replace('//', '/')}`
+        return `https://ghfast.top/${rawUrl.replace('//', '/')}`
       } else {
         return rawUrl
       }
